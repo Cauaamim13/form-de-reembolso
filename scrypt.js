675bebe5552360b875dcfe4e40b155e626e18e28
CauaAmim <cauaamim@gmail.com>
2026-02-06 14:22:02 -0300

    integration with n8n

--- scrypt.js
+++ scrypt.js
@@ -1,8 +1,40 @@
-const enviarDados = async (dados) => {
-  const response = await fetch('https://cauaamim.app.n8n.cloud/webhook-test/3b18419d-43dd-43ce-a2ca-4c7c90f9d69d', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(dados)
-  });
-  return response.json();
-};
+const formulario = document.querySelector('.formulário');
+
+formulario.addEventListener('submit', async (event) => {
+  
+    event.preventDefault();
+
+    
+    const nome = document.getElementById('nomeCliente').value;
+    const email = document.getElementById('emailCliente').value;
+    const motivo = document.getElementById('motivoReembolso').value;
+    
+    
+    const produto = document.querySelector('input[type="radio"]:checked')?.parentElement.textContent.trim() || "Não selecionado";
+
+    const dados = {
+        nome: nome,
+        email: email,
+        produto: produto,
+        motivo: motivo
+    };
+
+    try {
+        const response = await fetch('https://cauaamim.app.n8n.cloud/webhook-test/3b18419d-43dd-43ce-a2ca-4c7c90f9d69d', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(dados)
+        });
+
+        if (response.ok) {
+            alert('Solicitação enviada com sucesso!');
+        } else {
+            alert('Erro ao enviar.');
+        }
+    } catch (error) {
+        console.error('Erro de conexão:', error);
+        alert('Não foi possível conectar ao n8n. Verifique o console.');
+    }
+});
